790346c774ae996679a8ef376b7f687f24862848
Yoshi_0x <yoshihyoda@gmail.com>
2024-01-02 15:15:31 -0800

    fix minor issues

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -12,7 +12,7 @@ export const rubik_glitch = Rubik_Glitch({
 
 function Header() {
   return (
-    <header className="sticky top-0 z-10 mx-auto flex w-full items-center justify-between bg-white px-4 py-2 md:pt-8 xl:w-3/4 dark:bg-[#020816]">
+    <header className="mx-auto flex w-full items-center justify-between bg-white px-4 py-2 md:pt-8 xl:w-3/4 dark:bg-[#020816]">
       <Link href="/">
         <div className="flex cursor-pointer flex-row items-center justify-center">
           <Image src={Icon} height={60} width={60} alt="logo" />
